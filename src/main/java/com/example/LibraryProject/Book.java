59c59b6138ed1f62c5ea59a5c16e5d3ad6c192f5
Winzar <connorwinzar@gmail.com>
2022-06-29 22:12:23 +0100

    spring start

---
+++ src/main/java/com/example/LibraryProject/Book.java
@@ -0,0 +1,67 @@
+package com.example.LibraryProject;
+
+public class Book {
+    private int number;
+    private String title;
+    private String author;
+    private String genre;
+    private String subgenre;
+    private String publisher;
+
+    public Book(int number, String title, String author, String genre, String subgenre, String publisher) {
+        this.number = number;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.subgenre = subgenre;
+        this.publisher = publisher;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSubgenre() {
+        return subgenre;
+    }
+
+    public void setSubgenre(String subgenre) {
+        this.subgenre = subgenre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
---
+++ src/main/java/com/example/LibraryProject/CSVReader.java
@@ -0,0 +1,11 @@
+package com.example.LibraryProject;
+
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+
+@Component
+public class CSVReader {
+    private Map<String, Book> bookMap= new HashMap<>();
+}
